e115059b9e2764c9697336d7b201ef860cf4dccf
piyush <pmirase111@gmail.com>
2022-03-01 17:32:22 +0530

    hi i'm here

--- Git_Repo/src/Git_files.java
+++ Git_Repo/src/Git_files.java
@@ -8,4 +8,8 @@ public class Git_files {
 
 
 
-//right click --> project --> team --> share project
+// right click --> project --> team --> share project--> create --> 
+//create button to create new repo
+// right click project -->team -->add to index 
+//team ->>remote --> push--> fill uri ->> next --> source ref click drop down--> select man
+//click add  --> finish
